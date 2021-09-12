e1d71d9fd27712310419a2d840d51bf161e932db
Dawid Bańczak <dawid.banczak99@gmail.com>
2021-09-12 13:06:42 +0200

    Added enums and basic converters

--- 2DFirstGame/DrawingHandler/String/DrawCharacters.cs
+++ 2DFirstGame/DrawingHandler/String/DrawCharacters.cs
@@ -1,7 +1,25 @@
-﻿namespace _2DFirstGame.DrawingHandler.String
+﻿using System;
+
+namespace _2DFirstGame.DrawingHandler.String
 {
     public static class DrawCharacters
     {
-
+        public static Characters ConvertCharToCharacters(char character)
+        {
+            char ch = Char.ToLower(character);
+            int code = (int)ch - 97;
+            if (code < 0 || code > 122)
+            {
+                throw new Exception("TODO: handle special characters");
+            }
+            else
+            {
+                if (code == 0)
+                {
+                    return Characters.Z;
+                }
+                return (Characters)code;
+            }
+        }
     }
 }
--- 2DFirstGame/DrawingHandler/String/DrawNumbers.cs
+++ 2DFirstGame/DrawingHandler/String/DrawNumbers.cs
@@ -2,6 +2,16 @@
 {
     public static class DrawNumbers
     {
-
+        public static Numbers ConvertIntToNumbers(int number)
+        {
+            if (number == 0)
+            {
+                return Numbers.Zero;
+            }
+            else
+            {
+                return (Numbers)number - 1;
+            }
+        }
     }
 }
