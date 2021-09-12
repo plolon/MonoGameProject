aae404813ca4e0ed9c46415ccf97fffbc79f09e1
Dawid Bańczak <dawid.banczak99@gmail.com>
2021-09-12 12:45:48 +0200

    Initialized new class DrawingHelper and added constructor

---
+++ 2DFirstGame/DrawingHandler/DrawingHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2DFirstGame.DrawingHandler
+{
+    public class DrawingHelper
+    {
+        private Texture2D numbers;
+        private Texture2D characters;
+
+        public DrawingHelper(Texture2D numbers, Texture2D characters)
+        {
+            this.numbers = numbers;
+            this.characters = characters;
+        }
+    }
+}
