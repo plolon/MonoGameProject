97e3de8fa5ac1dcfec4f6b9752b6ecb67cf38c6c
Dawid Bańczak <dawid.banczak99@gmail.com>
2021-09-12 13:37:33 +0200

    Added number drawing logic

--- 2DFirstGame/DrawingHandler/DrawingHelper.cs
+++ 2DFirstGame/DrawingHandler/DrawingHelper.cs
@@ -1,8 +1,8 @@
 ﻿using _2DFirstGame.DrawingHandler.String;
 using _2DFirstGame.DrawingHandler.String.Utils;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System.Linq;
-using System.Numerics;
 
 namespace _2DFirstGame.DrawingHandler
 {
@@ -10,20 +10,25 @@ namespace _2DFirstGame.DrawingHandler
     {
         private Texture2D numbers;
         private Texture2D characters;
+        private SpriteBatch device;
 
         private float posX;
+        private float posY;
         private StringUtil util;
+        private float scale;
 
-        public DrawingHelper(Texture2D numbers, Texture2D characters)
+        public DrawingHelper(SpriteBatch device, Texture2D numbers, Texture2D characters)
         {
+            this.device = device;
             this.numbers = numbers;
             this.characters = characters;
             util = new StringUtil();
         }
-
-        public void DrawString(SpriteBatch device, Vector2 position, string text, float scale)
+        public void DrawString(Vector2 position, string text, float scale)
         {
             posX = position.X;
+            posX = position.Y;
+            this.scale = scale;
             text.ToList().ForEach(x => HandleLetter(x));
         }
 
@@ -33,10 +38,13 @@ namespace _2DFirstGame.DrawingHandler
             if (int.TryParse(character.ToString(), out num))    // number
             {
                 Numbers code = DrawNumbers.ConvertIntToNumbers(num);
+                Rectangle rect = util.getRect(code);
+                DrawNumbers.Draw(device, numbers, rect, new Vector2(posX, posY), scale);
             }
             else if (true)  // character
             {
                 Characters code = DrawCharacters.ConvertCharToCharacters(character);
+                Rectangle rect = util.getRect(code);
             }
             else    // whitespace
             {
--- 2DFirstGame/DrawingHandler/String/DrawNumbers.cs
+++ 2DFirstGame/DrawingHandler/String/DrawNumbers.cs
@@ -1,4 +1,7 @@
-﻿namespace _2DFirstGame.DrawingHandler.String
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace _2DFirstGame.DrawingHandler.String
 {
     public static class DrawNumbers
     {
@@ -12,6 +15,10 @@
             {
                 return (Numbers)number - 1;
             }
+        }
+        public static void Draw(SpriteBatch spriteBatch, Texture2D texture, Rectangle rect, Vector2 position, float? scale)
+        {
+            spriteBatch.Draw(texture, position, rect, Color.White, 0f, new Vector2(0, 0), scale.Value, SpriteEffects.None, 0f);
         }
     }
 }
