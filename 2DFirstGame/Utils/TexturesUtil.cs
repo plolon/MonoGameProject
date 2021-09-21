db11362ce78f8e46f178e88eb9f994f9503aa92f
Dawid Bańczak <dawid.banczak@mipcloud.pl>
2021-09-21 13:22:09 +0200

    Added inside walls texture and texture util methods

--- 2DFirstGame/Utils/TexturesUtil.cs
+++ 2DFirstGame/Utils/TexturesUtil.cs
@@ -8,10 +8,12 @@ namespace _2DFirstGame.Utils
     {
         private List<Rectangle> walls;
         private List<Rectangle> grounds;
+        private List<Rectangle> inside_walls;
 
         public TexturesUtil()
         {
             walls = GetWallSource();
+            inside_walls = GetInside_WallSource();
             grounds = new List<Rectangle>() { new Rectangle(0, 0, 64, 64) };
         }
 
@@ -34,6 +36,15 @@ namespace _2DFirstGame.Utils
             }
             return result;
         }
+        private List<Rectangle> GetInside_WallSource()
+        {
+            List<Rectangle> result = new List<Rectangle>();
+            for(int i=0; i<323; i += 64)
+            {
+                result.Add(new Rectangle(i, 0, 64, 64));
+            }
+            return result;
+        }
     }
 
     public enum Walls
@@ -46,6 +57,13 @@ namespace _2DFirstGame.Utils
         Left_Down,
         Right_Up,
         Right_Down
+    }
+    public enum Inside_Walls
+    {
+        Left,
+        Right,
+        Up,
+        Down,
     }
     public enum Grounds
     {
