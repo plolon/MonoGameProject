4e701b5899e99084e97dcf99259bd30a7d82bdcb
Dawid Bańczak <dawid.banczak@mipcloud.pl>
2021-10-04 15:06:58 +0200

    Basic hearth drawing

---
+++ 2DFirstGame/DrawingHandler/HUD.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace _2DFirstGame.DrawingHandler
+{
+    public static class HUD
+    {
+        public static void DrawHUD(SpriteBatch device, Player player)
+        {
+            DrawHP(device, player.HP);
+        }
+        private static void DrawHP(SpriteBatch device, int hp)
+        {
+            bool half;
+            int solid = hp / 2;
+            if (hp % 2 != 0)
+                half = true;
+            else
+                half = false;
+            for(int i=0; i<5; i++)
+            {
+                if (i <= solid)
+                {
+
+                }
+                else if (half)
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+        }
+    }
+}
